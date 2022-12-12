12a96441355a99a1c89123e832895389b0013624
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 11:06:02 -0300

    [url] (cars) registre users routes

--- cars/cars/urls.py
+++ cars/cars/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from ninja import NinjaAPI
 
 from vehicles.views import VehiclesViews
+from users.views import UsersViews
 
 api = NinjaAPI()
 
 api.add_router('/vehicles/', VehiclesViews.router)
+api.add_router('/users/', UsersViews.router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
