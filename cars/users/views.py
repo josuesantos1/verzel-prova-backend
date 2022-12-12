386cc4b48f915561e73e8efaa153d7e5da178680
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 11:06:27 -0300

    [views] (users) handlers base

--- cars/users/views.py
+++ cars/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from ninja import Router
 
-# Create your views here.
+class UsersViews:
+
+    router = Router()
+
+    @router.post('/')
+    def create(request):
+        return {'message': 'created'}
+
+    @router.get('/me')
+    def view(request):
+        return 'ola'
+
+    @router.put('/me')
+    def update(request):
+        return {'message': 'updated'}
+
+    @router.delete('/me')
+    def delete(request): 
+        return {'message': 'deleted'}
+
