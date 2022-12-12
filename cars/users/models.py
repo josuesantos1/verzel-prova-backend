b374414f15fce733e9f6792dde70d51965b66425
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 11:45:04 -0300

    [models] (users)

--- cars/users/models.py
+++ cars/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    email = models.CharField(
+        max_length=260,
+        null=False,
+        blank=False,
+    )
+
+    password = models.TextField(
+        null=False,
+        blank=False,
+    )
