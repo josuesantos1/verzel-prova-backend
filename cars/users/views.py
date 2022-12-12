a333f6c2de06a3a6da993693c2acd22b6205e968
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 11:45:51 -0300

    [views] (users) create user handlers

--- cars/users/views.py
+++ cars/users/views.py
@@ -1,12 +1,28 @@
+import crypt
+
 from ninja import Router
+from django.forms.models import model_to_dict
+from ninja.errors import HttpError
+
+from .models import Users
+from .schemas import UserSchema
 
 class UsersViews:
 
     router = Router()
 
     @router.post('/')
-    def create(request):
-        return {'message': 'created'}
+    def create(request, data: UserSchema):
+        user = data.dict()
+
+        if Users.objects.filter(email=user['email']).exists():
+            raise HttpError(400, 'already exists')
+            
+        user['password'] = crypt.crypt(user['email']) 
+        users = Users(**user)
+        users.save()
+
+        return model_to_dict(users)
 
     @router.get('/me')
     def view(request):
