3a23ca05d9309e8a9706d01ccf5bbca96ba7c483
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 10:08:13 -0300

    [urls] (app) config ninja routes

--- cars/cars/urls.py
+++ cars/cars/urls.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 
+from vehicles.views import VehiclesViews
+
 api = NinjaAPI()
+
+api.add_router('/vehicles/', VehiclesViews.router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
