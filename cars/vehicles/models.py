ddf06e860ad0b8f3574743320a7e6a70ee72d04a
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 09:53:54 -0300

    [models] (vehicles)

--- cars/vehicles/models.py
+++ cars/vehicles/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Vehicles(models.Model):
+    id = models.AutoField(
+        primary_key=True
+    )
+
+    name = models.CharField(
+        max_length=150,
+        null=False,
+        blank=False
+    )
+
+    brand = models.CharField(
+        max_length=32,
+        null=False,
+        blank=False
+    )
+
+    description = models.TextField(
+        max_length=10240,
+    )
+
+    price = models.IntegerField()
+
+    sold = models.BooleanField(
+        default=False
+    ) 
+    
+
+class VehiclesImages(models.Model):
+    vehicles = models.ForeignKey(
+        Vehicles, on_delete=models.CASCADE
+    )
+
+    key = models.CharField(
+        max_length=64,
+        null=False,
+        blank=False
+    )
