b546b6f3b43a68a44ddb1f8350927a9aac4cab6a
Josué Santos <josue.santos.dev@gmail.com>
2022-12-12 09:26:56 -0300

    [views] (vehicles) handlers base

--- cars/vehicles/views.py
+++ cars/vehicles/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from ninja import Router
 
-# Create your views here.
+from .schemas import VehiclesSchema
+
+class VehiclesViews():
+    router = Router()
+
+    @router.post('/')
+    def create(req, data: VehiclesSchema):
+        data = data.dict()
+
+        return {'result': 'vehicles'}
+
+    @router.get('/')
+    def viewAll(req):
+        return {'result': 'vehicles'}
+
+    @router.get('/me')
+    def viewAllMe(req):
+        return {'result': 'vehicles'}
+
+    @router.get('/')
+    def view(req):
+        return {'result': 'vehicles'}
+
+    @router.put('/')
+    def update(req):
+        return {'result': 'vehicles'}
+
+    @router.get('/')
+    def delete(req):
+        return {'result': 'vehicles'}
+
+    # images 
+    @router.post('/images')
+    def create_image(req):
+        return {'result': 'vehicles'}
+
+    @router.get('/images')
+    def view_image(req):
+        return {'result': 'vehicles'}
+
+    @router.put('/images')
+    def update(req):
+        return {'result': 'vehicles'}
+
+    @router.delete('/images')
+    def delete(req):
+        return {'result': 'vehicles'}
+
+
